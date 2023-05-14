1fc1c46fa6e535dda8142a703fedd561913d88c6
Liliana Strajescu <lilly.milan96@gmail.com>
2023-05-14 17:30:30 +0000

    Update 2 files

--- src/Weather.js
+++ src/Weather.js
@@ -4,7 +4,7 @@ import axios from "axios";
 export default function Weather(props) {
   let [temperature, setTemperature] = useState(null);
   let apiKey = "f3009e4852fa0a079dab291dabf020c4";
-  let url = `https://api.openweathermap.org/data/2.5/weather?q=${props.city}&appid=${apiKey}&units&metric`;
+  let url = `https://api.openweathermap.org/data/2.5/weather?q=${props.city}&appid=${apiKey}&units=metric`;
   axios.get(url).then(showTemperature);
 
   function showTemperature(response) {
